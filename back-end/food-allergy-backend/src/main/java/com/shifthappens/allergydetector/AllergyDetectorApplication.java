22bdf5f407ad36d7c888cae361b5f0a93864994d
yasas <yasashwini.napa@gmail.com>
2024-10-19 13:08:18 -0400

    Initial commit

---
+++ back-end/food-allergy-backend/src/main/java/com/shifthappens/allergydetector/AllergyDetectorApplication.java
@@ -0,0 +1,13 @@
+package com.shifthappens.allergydetector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllergyDetectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllergyDetectorApplication.class, args);
+	}
+
+}
