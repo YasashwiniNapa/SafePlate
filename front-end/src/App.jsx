6636e30b19e693236498c257882fc5f55b1594dd
yasas <yasashwini.napa@gmail.com>
2024-10-19 13:04:03 -0400

    Initial commit

---
+++ front-end/src/App.jsx
@@ -0,0 +1,17 @@
+
+
+function App() {
+  
+      
+    return(
+      <>
+
+      <section id="core-concepts">
+      <h2>Core Concepts</h2>
+      </section>
+      </>
+
+    );
+}
+
+export default App;
