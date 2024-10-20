c98f00b6b40c4970cbd38048991742e335a9eb7d
yasas <yasashwini.napa@gmail.com>
2024-10-20 06:26:41 -0400

    lifted state identifiedAllergies

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,15 +1,18 @@
-import React from 'react';
-import Camera from './components/Camera';
+import React, { useState } from 'react';
 import ImageUpload from './components/ImageUpload'; // Adjust the path as necessary
 import Results from './components/Results'; // Adjust the path as necessary
 import Profile from './components/Profile'; // Adjust the path as necessary
+import Camera from './components/Camera';
 
 const App = () => {
+  const [identifiedAllergies, setIdentifiedAllergies] = useState([]);
+
   return (
     <div className="min-h-screen bg-gradient-to-br from-[#5CA135] to-[#196E63] bg-cover bg-fixed">
       <main>
         <Profile />
         <ImageUpload />
+        <Camera setIdentifiedAllergies={setIdentifiedAllergies} />
         <Results />
       </main>
     </div>
