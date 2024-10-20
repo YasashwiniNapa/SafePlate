6fbdb86ee9b9506b8e4c6496a77bd14f829c636b
Michelle Li <michelleli1510@gmail.com>
2024-10-20 02:28:51 -0400

    App, Profile.jsx

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -4,10 +4,11 @@ import Camera from './components/Camera';
 import ImageUpload from './components/ImageUpload'; // Adjust the path as necessary
 import Results from './components/Results'; // Adjust the path as necessary
 import Profile from './components/Profile'; // Adjust the path as necessary
+import './index.css'
 
 const App = () => {
   return (
-    <div>
+    <div className="min-h-screen bg-gradient-to-br from-[#5CA135] to-[#196E63] bg-cover bg-fixed">
       <main>
         <Profile />
         <ImageUpload />
